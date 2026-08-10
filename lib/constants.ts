a2f6be264b1200a0d96da367f437349695383cb8
sherry-part <ccc_7020@qq.com>
2026-08-10 13:46:48 +0800

    chore: delete some footer text.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1098,8 +1098,8 @@ export const ANNOUNCEMENT = {
 
 export const FOOTER = {
   description: {
-    en: 'An operating system for embodied intelligence. Let robots understand you and act for you.',
-    zh: '具身智能操作系统。让机器人懂你所想，行你所愿。',
+    en: 'Let robots understand you and act for you.',
+    zh: '让机器人懂你所想，行你所愿。',
   },
   product: [
     { label: { en: 'Why', zh: '问题' }, href: '#why' },
